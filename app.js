e3b25a2770740f4446b00b15d29bd83a2f9630c8
avdleest <avdleest@gmail.com>
2019-09-06 18:22:51 +0200

    created controller and model

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const http = require('http')
-const mongoose = require('mongoose')
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
+const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+
+app.use('/api/blogs', blogsRouter)
 
 const mongoUrl = 'mongodb+srv://fullstack:fshizzle@cluster0-ip5pk.mongodb.net/blog-app?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
-
-app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
