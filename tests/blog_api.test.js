ae1c2d15db2a6ec474183269aa4180ac0a9d6679
avdleest <avdleest@gmail.com>
2019-09-08 22:07:52 +0200

    4.10 completed

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,55 +3,63 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-const initialBlogs = [
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-  },
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-  },
-  {
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-  }
-]
+const helper = require('./test_helper')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
   const promiseArray = blogObjects.map(blog => blog.save())
   await Promise.all(promiseArray)
 
 })
 
-test('blogs are retured as JSON', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('blog retrieval correct and correctly formatted', () => {
+
+  test('blogs are retured as JSON', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('the amount of blogs is the correct length', async () => {
+    const data = await api.get('/api/blogs')
+    expect(data.body.length).toBe(helper.initialBlogs.length)
+  })
+
+  test('unique identifier property is named id', async () => {
+    const data = await api.get('/api/blogs')
+    expect(data.body[0].id).toBeDefined()
+  })
 })
 
-test('the amount of blogs is the correct length', async () => {
-  const data = await api.get('/api/blogs')
-  expect(data.body.length).toBe(3)
-})
+describe('posting a new blog', () => {
+  test('a valid new blog can be added', async () => {
+    newTitle = "Great ways for testing"
+    const newBlog = new Blog({
+      title: newTitle,
+      author: "Unknown author",
+      url: "http://www.letmegooglethatforyou.com"
+    })
 
-test('unique identifier property is named id', async () => {
-  const data = await api.get('/api/blogs')
-  expect(data.body[0].id).toBeDefined()
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    console.log(`blogs at end: ${blogsAtEnd.length}`)
+    expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+
+    const contents = blogsAtEnd.map(blogs => blogs.title)
+    expect(contents).toContain(newTitle)
+  })
 })
 
 
 afterAll(() => {
-  mongoose.connection.close()
+  mongoose.disconnect()
+  //mongoose.connection.close()
 })
