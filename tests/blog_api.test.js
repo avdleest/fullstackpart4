3f4930ccef494427336504c311b426b361ff2f81
avdleest <avdleest@gmail.com>
2019-09-08 19:18:05 +0200

    Redefined env and defined a few tests. Before implementing async/await

---
+++ tests/blog_api.test.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+
+})
+
+test('blogs are retured as JSON', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('the amount of blogs is the correct length', async () => {
+  const data = await api.get('/api/blogs')
+  expect(data.body.length).toBe(3)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,38 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  } else {
+    let likes = 0
+    blogs.forEach(element => {
+      likes += element.likes
+    })
+    return likes
+  }
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  } else {
+    let mostLikes = 0
+    let bestBlog = {}
+    blogs.forEach(element => {
+      if (element.likes >= mostLikes) {
+        mostLikes = element.likes
+        bestBlog = {
+          title: element.title,
+          author: element.author,
+          likes: element.likes
+        }
+      }
+    })
+    return bestBlog
+  }
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes, favoriteBlog
 }
