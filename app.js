aba805161f5a50c20098cf35402368a0ab0161ac
avdleest <avdleest@gmail.com>
2019-10-07 11:46:32 -0500

    All exercises for part 4 completed

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 
 const mongoUrl = config.MONGODB_URI
@@ -13,8 +15,14 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,52 +1,69 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
-    .find({}).populate('users', { name: 1, username: 1 })
+    .find({}).populate('user', { name: 1, username: 1 })
   response.json(blogs.map(b => b.toJSON()))
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  const user = await User.findById(body.userId)
 
-  if (!body.userId) {
-    response.status(400).json({ 'error:': 'UserId not provided' })
-  } else if (user === null) {
-    response.status(404).json({ 'error:': 'Provided user not found' })
-  }
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+      response.status(401).json({ error: 'token missing or invalid' })
+    }
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-    user: user._id
-  })
+    const user = await User.findById(decodedToken.id)
 
-  console.log(blog)
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: user.id
+    })
 
-  if (blog.title === undefined && blog.url == undefined) {
-    response.status(400).json({ 'error:': 'Blog title and url not defined' })
-  } else {
-    try {
+    if (blog.title === undefined && blog.url == undefined) {
+      response.status(400).json({ 'error:': 'Blog title and url not defined' })
+    } else {
       const savedBlog = await blog.save()
-      console.log(savedBlog)
       user.blogs = user.blogs.concat(savedBlog.id)
       await user.save()
       response.status(201).json(savedBlog.toJSON())
-    } catch (exception) {
-      next(exception)
     }
+  } catch (exception) {
+    next(exception)
   }
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
   try {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+      response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const blog = await Blog.findById(request.params.id)
+
+    if (!blog) response.status(404).json({ error: `No blog found with id ${request.params.id}` })
+
+    if (blog.user.toString() === user.id.toString()) {
+      blog.remove()
+      response.status(204).end()
+    } else {
+      response.status(403).json({ error: 'user not allowed to remove other than own blogs' })
+    }
+
+    // await Blog.findByIdAndRemove(request.params.id)
+    // response.status(204).end()
   } catch (exception) {
     next(exception)
   }
